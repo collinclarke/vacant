e4846cf285f75c72353a822236ceede572b2dbac
Collin <collinclarke@gmail.com>
2017-10-24 12:33:12 -0400

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/bench_bnb.jsx",
+  output: {
+    path: path.join(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", '.jsx', '*']
+  }
+};
