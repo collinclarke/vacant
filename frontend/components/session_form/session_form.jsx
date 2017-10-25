a4feaff2d5cdcc736ca2aed2d2f2a504859e7f6a
Collin <collinclarke@gmail.com>
2017-10-25 13:07:54 -0400

    Implement birthday selector in signup

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -9,7 +9,9 @@ class SessionForm extends Component {
       password: "",
       first_name: "",
       last_name: "",
-      birth_date: ""
+      birth_day: "Day",
+      birth_month: "Month",
+      birth_year: "Year",
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -22,12 +24,40 @@ class SessionForm extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    const user = Object.assign({}, this.state);
+    const user = Object.assign({}, this.state, {
+      birth_date: this.birthday()
+    });
     this.props.processForm(user);
   }
 
   update(field) {
     return e => this.setState({ [field]: e.currentTarget.value });
+  }
+
+  birthday() {
+    const {birth_day, birth_month, birth_year} = this.state;
+    const birthday = `${birth_year}-${birth_month}-${birth_day}`;
+    return birthday;
+  }
+
+  dates() {
+    let dates = [];
+    const now = new Date();
+    for (let i=now.getFullYear(); i >= 1920 ; i--) {
+      dates.push(<option key={i} value={`${i}`}>{ i }</option>);
+    }
+    return dates;
+  }
+
+  days() {
+    let days = [];
+    for (let i=1; i <= 9; i++) {
+      days.push(<option key={i} value={`0${i}`}>{ i }</option>);
+    }
+    for (let i=10; i <= 31; i++) {
+      days.push(<option key={i} value={`${i}`}>{ i }</option>);
+    }
+    return days;
   }
 
   signupFields() {
@@ -37,19 +67,19 @@ class SessionForm extends Component {
         <div className="session-form-input">
           <input id="email" type="text" value={ this.state.email }
             onChange={ this.update('email') } placeholder="Email Address"/>
-          <i className="icon ion-ios-email-outline i-email"></i>
+          <i id="i-email" className="icon ion-ios-email-outline"></i>
         </div>
 
         <div className="session-form-input">
           <input id="first-name" type="text" value={ this.state.first_name }
             onChange={ this.update('first_name') } placeholder="First Name"/>
-          <i className="icon ion-ios-user-outline i-email"></i>
+          <i className="icon ion-ios-person-outline i-email"></i>
         </div>
 
         <div className="session-form-input">
           <input id="last-name" type="text" value={ this.state.last_name }
             onChange={ this.update('last_name') } placeholder="Last Name"/>
-          <i className="icon ion-ios-user-outline i-email"></i>
+          <i className="icon ion-ios-person-outline i-email"></i>
         </div>
 
         <div className="session-form-input">
@@ -58,11 +88,38 @@ class SessionForm extends Component {
           <i className="icon ion-ios-locked-outline i-pw"></i>
         </div>
 
-        <label>Birthday</label>
-        <div className="session-form-input">
-          <input id="birthdate" type="date" value={ this.state.birth_date }
-            onChange={ this.update('birth_date') }/>
-          <i className="icon ion-ios-email-outline i-email"></i>
+        <label className="loose">Birthday</label>
+        <p className="loose">To sign up, you must be 18 or older. Other people won't see your birthday</p>
+        <div className="session-form-birthday">
+          <label className="select">
+            <select id="birth-month" defaultValue="Month" onChange={ this.update('birth_month') }>
+              <option value="Month" disabled>Month</option>
+              <option value="01">January</option>
+              <option value="02">February</option>
+              <option value="03">March</option>
+              <option value="04">April</option>
+              <option value="05">May</option>
+              <option value="06">June</option>
+              <option value="07">July</option>
+              <option value="08">August</option>
+              <option value="09">September</option>
+              <option value="10">October</option>
+              <option value="11">November</option>
+              <option value="12">December</option>
+            </select>
+          </label>
+          <label className="select">
+            <select id="birth-day" defaultValue="Day" onChange={ this.update('birth_day') }>
+              <option value="Day" disabled>Day</option>
+              { this.days() }
+            </select>
+          </label>
+          <label className="select">
+            <select id="birth-year" defaultValue="Year" onChange={ this.update('birth_year') }>
+              <option value="Year" disabled>Year</option>
+              { this.dates() }
+            </select>
+          </label>
         </div>
       </section>
     );
