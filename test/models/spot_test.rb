00c0bd4fd6625ef4da87c0d33be3bc9af1fb02d3
Collin <collinclarke@gmail.com>
2017-10-26 12:13:13 -0400

    Add rails generated test files

---
+++ test/models/spot_test.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: spots
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  address    :string           not null
+#  price      :string           not null
+#  type       :string           not null
+#  host_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'test_helper'
+
+class SpotTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer          not null, primary key
+#  email              :string           not null
+#  first_name         :string           not null
+#  last_name          :string           not null
+#  password_digest    :string           not null
+#  session_token      :integer          not null
+#  birth_date         :date             not null
+#  currency           :string
+#  language           :string
+#  description        :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
