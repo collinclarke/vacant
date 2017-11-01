9b321686b6116bf35cf75a37ef501d1cee8babd4
Collin <collinclarke@gmail.com>
2017-11-01 10:51:39 -0400

    Add splashpage framework

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,6 +5,7 @@ import SessionFormContainer from './session_form/session_form_container';
 import SpotsSearchContainer from './spots_index/spots_search_container';
 import SpotShowContainer from './spot_show/spot_show_container';
 import BookingsIndexContainer from './bookings_index/bookings_index_container';
+import SplashPage from './splash/splash_page';
 import { AuthRoute } from '../util/route_util';
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <a className="main-logo" href="/#/"><img src={ window.logoUrl } alt="Vacant Logo"/></a>
         <GreetingContainer />
       </header>
+      <Route exact path='/' component={ SplashPage } />
       <Route exact path='/spots' component={ SpotsSearchContainer } />
       <Route exact path='/bookings' component={ BookingsIndexContainer } />
       <Route path='/spots/:spotId' component={ SpotShowContainer } />
