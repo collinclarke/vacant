6868b41398ee4752528af68c32a8ff9d2dca0bfe
Collin <collinclarke@gmail.com>
2017-11-01 15:54:21 -0400

    Fix splash page visibility

--- frontend/components/splash/splash_page.jsx
+++ frontend/components/splash/splash_page.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 
 export default () => {
   return (
-    <section className="splash-page">
+    <section className="splash-page full-screen">
       <Link to='/spots'>Spots</Link>
     </section>
   );
-}
+};
