e336b71065a68f78fe60ff5469e99ac9567f16cd
Collin <collinclarke@gmail.com>
2017-11-20 12:30:40 -0500

    Add responsive rules

--- frontend/components/splash/splash_page.jsx
+++ frontend/components/splash/splash_page.jsx
@@ -5,8 +5,17 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <section className="splash-page full-screen">
-      <Link to='/spots'><img src={ window.logoUrl } alt="Vacant Logo"/>vacant</Link>
-      <div>A place for<br /> creatives to find space.</div>
+      <Link to='/spots'>
+        <div id="splash-logo">
+          <img src={ window.logoUrl } alt="Vacant Logo"/>
+          <span>vacant</span>
+        </div>
+      </Link>
+
+      <div id="splash-tag">
+        A marketplace for unused real estate in NYC
+      </div>
+
     </section>
   );
 };
